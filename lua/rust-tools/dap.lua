001933c94efe7968b4de2194ebf8e1bd2f60d602
Maxime <maximemikotajewski@gmail.com>
2025-05-18 13:56:41 +0200

    Fixed LUA crash due to some invalid default value

--- lua/rust-tools/dap.lua
+++ lua/rust-tools/dap.lua
@@ -61,7 +61,7 @@ local function get_cargo_args_from_runnables_args(runnable_args)
         table.insert(cargo_args, message_json)
     end
 
-    for _, value in ipairs(runnable_args.cargoExtraArgs) do
+    for _, value in ipairs(runnable_args.cargoExtraArgs or {}) do
         if not rt.utils.contains(cargo_args, value) then
             table.insert(cargo_args, value)
         end
